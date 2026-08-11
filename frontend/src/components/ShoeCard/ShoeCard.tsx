38ef752f235a6057c56ef1d968461d40cfe1ae61
Nazar Baranov <baranovnazar97@gmail.com>
2026-08-11 19:11:47 +0300

    update shoe card layout and styles

--- frontend/src/components/ShoeCard/ShoeCard.tsx
+++ frontend/src/components/ShoeCard/ShoeCard.tsx
@@ -27,13 +27,18 @@ const ShoeCard = ({ item, addToCart }: ShoeProp) => {
         />
       </div>
       <div className="product-info">
-        <h2>{item.title}</h2>
-        <h2>{item.brand}</h2>
-        <h3>{item.category}</h3>
-        <h2 className="card-price">{item.price}</h2>
-        <div className="product-rating">
-          <img src={star} alt="star" />
-          <p>{item.rating}</p>
+        <div className="product-descr">
+          <div className="card-header-row">
+            <h2>{item.title}</h2>
+            <div className="product-rating">
+              <img src={star} alt="star" />
+              <p>{item.rating}</p>
+            </div>
+          </div>
+
+          <span className="card-brand">{item.brand}</span>
+          <span className="card-category">{item.category}</span>
+          <p className="card-price">{item.price}</p>
         </div>
       </div>
       <div className="card-buttons">
